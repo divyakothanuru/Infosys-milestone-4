9f96d03f86111aafa2bc9837c9af2306e80adc03
divyakothanuru <kothanurud@gmail.com>
2026-02-04 09:27:16 +0530

    Add project structure for milestone 4 web app

---
+++ project structure.py
@@ -0,0 +1,11 @@
+milestone4_web_app/
+│
+├── app.py                  # Main Flask app
+├── model.pkl               # Trained ML model
+├── requirements.txt        # Dependencies
+│
+├── templates/
+│   └── index.html          # Frontend UI
+│
+└── static/
+    └── style.css           # Optional styling
